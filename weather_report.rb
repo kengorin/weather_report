dc2be7f794cae039102ab040ff3d6a758b434d93
kengorin <kengorin.net@gmail.com>
2016-11-08 19:19:58 +0900

    tokyo weather

--- weather_report.rb
+++ weather_report.rb
@@ -11,17 +11,33 @@ class WeatherReport
     end
 
     def exec
-      url = 'http://weather.yahoo.co.jp/weather/jp/36/7110.html'
-      date_path = "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[1]/text()"
-      forecast_path = "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[2]/text()"
-      forecast_image_path = "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[2]/img"
-      parsed_html = visit(url)
-      date = get_elements(parsed_html, date_path).first.to_s
-      forecast = get_elements(parsed_html, forecast_path).first.to_s
-      forecast_image_url = get_elements(parsed_html, forecast_image_path).first.attributes['src'].value
+      url1 = 'http://weather.yahoo.co.jp/weather/jp/36/7110.html'
+      url2 = 'http://weather.yahoo.co.jp/weather/13/4410.html'
+
+      date_path_1 = "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[1]/text()"
+      forecast_path_1 = "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[2]/text()"
+      forecast_image_path_1= "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[2]/img"
+
+      date_path_2 = "//*[@id=\"main\"]/div[7]/table/tr/td/div/p[1]/text()"
+      forecast_path_2 = "//*[@id=\"main\"]/div[7]/table/tr/td/div/p[2]/text()"
+      forecast_image_path_2= "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[2]/img"
+
+      parsed_html_1 = visit(url1)
+      parsed_html_2 = visit(url2)
+
+      date1 = get_elements(parsed_html_1, date_path_1).first.to_s
+      forecast1 = get_elements(parsed_html_1, forecast_path_1).first.to_s
+      forecast_image_url1 = get_elements(parsed_html_1, forecast_image_path_1).first.attributes['src'].value
+      date2 = get_elements(parsed_html_2, date_path_2).first.to_s
+      forecast2 = get_elements(parsed_html_2, forecast_path_2).first.to_s
+      forecast_image_url2 = get_elements(parsed_html_2, forecast_image_path_2).first.attributes['src'].value
+
       CSV.open("./weather.csv", 'w') do |csv|
-        csv.puts ['日付','徳島の天気', 'お天気像URL']
-        csv.puts [date,forecast, forecast_image_url]
+        csv.puts ['日付','徳島の天気', 'お天気像url1']
+        csv.puts [date1, forecast1, forecast_image_url1]
+        csv.puts []
+        csv.puts ['日付','東京の天気', 'お天気像url1']
+        csv.puts [date2, forecast2, forecast_image_url2]
       end
     end
 
