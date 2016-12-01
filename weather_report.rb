ba24d3f76c0127d32fd3fff01bab92ec464b9536
kengorin <kengorin.net@gmail.com>
2016-12-01 15:14:18 +0900

    get Xpath from CSV

--- weather_report.rb
+++ weather_report.rb
@@ -5,40 +5,64 @@ require 'csv'
 require 'open-uri'
 require 'kconv'
 
-class WeatherReport
-    def initialize
+class WeatherReport #WeatherReportクラスの定義
+    def initialize  #初期化メソッド
       @user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
     end
 
     def exec
-      url1 = 'http://weather.yahoo.co.jp/weather/jp/36/7110.html'
-      url2 = 'http://weather.yahoo.co.jp/weather/13/4410.html'
 
-      date_path_1 = "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[1]/text()"
-      forecast_path_1 = "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[2]/text()"
-      forecast_image_path_1= "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[2]/img"
+      #url = ['http://weather.yahoo.co.jp/weather/jp/36/7110.html', 'http://weather.yahoo.co.jp/weather/13/4410.html']
+      #city = ['徳島','東京']  #URLと地域の配列を定義
 
-      date_path_2 = "//*[@id=\"main\"]/div[6]/table/tr/td/div/p[1]/text()"
-      forecast_path_2 = "//*[@id=\"main\"]/div[6]/table/tr/td/div/p[2]/text()"
-      forecast_image_path_2= "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[2]/img"
-
-      parsed_html_1 = visit(url1)
-      parsed_html_2 = visit(url2)
-
-      date1 = get_elements(parsed_html_1, date_path_1).first.to_s
-      forecast1 = get_elements(parsed_html_1, forecast_path_1).first.to_s
-      forecast_image_url1 = get_elements(parsed_html_1, forecast_image_path_1).first.attributes['src'].value
-      date2 = get_elements(parsed_html_2, date_path_2).first.to_s
-      forecast2 = get_elements(parsed_html_2, forecast_path_2).first.to_s
-      forecast_image_url2 = get_elements(parsed_html_2, forecast_image_path_2).first.attributes['src'].value
-      #binding.pry
+      #url1 = 'http://weather.yahoo.co.jp/weather/jp/36/7110.html'
+      #url2 = 'http://weather.yahoo.co.jp/weather/13/4410.html'
       CSV.open("./weather.csv", 'w') do |csv|
-        csv.puts ['日付','徳島の天気', 'お天気像url1']
-        csv.puts [date1, forecast1, forecast_image_url1]
 
-        csv.puts ['日付','東京の天気', 'お天気像url1']
-        csv.puts [date2, forecast2, forecast_image_url2]
+        CSV.foreach('./xpath.csv', headers: true, quote_char: "¥") do |data|  #dataの行数だけループする
+
+
+            date_path_1 = data["Date_path"]#日付のXpathを代入
+            forecast_path_1 = data["Forecast_path"] #予報のXpathを代入
+            forecast_image_path_1= data["Forecast_Image_path"]#画像のXpathを代入
+
+            parsed_html_1 = visit(data["URL"]) #URL読み込み
+
+
+            date1 = get_elements(parsed_html_1, date_path_1).first.to_s
+            forecast1 = get_elements(parsed_html_1, forecast_path_1).first.to_s
+            forecast_image_url1 = get_elements(parsed_html_1, forecast_image_path_1).first.attributes['src'].value
+            #binding.pry
+            #CSV.open("./weather.csv", 'w') do |csv|
+            csv.puts ['日付',data["City"]+"の天気", 'お天気像url1']
+            csv.puts [date1, forecast1, forecast_image_url1]
+          #end
+
+
+        end
       end
+
+      #date_path_2 = "//*[@id=\"main\"]/div[6]/table/tr/td/div/p[1]/text()"
+      #forecast_path_2 = "//*[@id=\"main\"]/div[6]/table/tr/td/div/p[2]/text()"
+      #forecast_image_path_2= "//*[@id=\"main\"]/div[@class='forecastCity']/table/tr/td/div/p[2]/img"
+
+      #parsed_html_1 = visit(url1)
+      #parsed_html_2 = visit(url2)
+
+      #date1 = get_elements(parsed_html_1, date_path_1).first.to_s
+      #forecast1 = get_elements(parsed_html_1, forecast_path_1).first.to_s
+      #forecast_image_url1 = get_elements(parsed_html_1, forecast_image_path_1).first.attributes['src'].value
+
+      #date2 = get_elements(parsed_html_2, date_path_2).first.to_s
+      #forecast2 = get_elements(parsed_html_2, forecast_path_2).first.to_s
+      #forecast_image_url2 = get_elements(parsed_html_2, forecast_image_path_2).first.attributes['src'].value
+      #CSV.open("./weather.csv", 'w') do |csv|
+        #csv.puts ['日付','徳島の天気', 'お天気像url1']
+        #csv.puts [date1, forecast1, forecast_image_url1]
+
+        #csv.puts ['日付','東京の天気', 'お天気像url1']
+        #csv.puts [date2, forecast2, forecast_image_url2]
+      #end
     end
 
     def visit(url)
